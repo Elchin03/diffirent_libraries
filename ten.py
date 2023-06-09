2d2e9ef04a63d48d2ecd2dad97f416a0158aa99e
Elchin03 <elcin03@mail.ru>
2023-06-09 21:04:05 +0300

    ten

--- ten.py
+++ ten.py
@@ -6,48 +6,53 @@ class StateMachine:
     def __init__(self):
         self.pose = "A"
 
-    def start(self):
+    def mix(self):
         if self.pose == "A":
             self.pose = "B"
             return 0
         if self.pose == "B":
             self.pose = "C"
-            return 1
-        if self.pose == "C":
-            self.pose = "A"
-            return 3
-        if self.pose == "F":
-            self.pose = "A"
-            return 8
-        raise MealyError("start")
-
-    def stay(self):
-        if self.pose == "F":
-            self.pose = "B"
-            return 7
-        if self.pose == "D":
-            self.pose = "E"
-            return 4
-        if self.pose == "E":
-            self.pose = "F"
-            return 5
-        if self.pose == "G":
-            self.pose = "A"
-            return 9
-        raise MealyError("stay")
-
-    def order(self):
-        if self.pose == "C":
-            self.pose = "D"
             return 2
+        if self.pose == "E":
+            self.pose = "C"
+            return 7
         if self.pose == "F":
             self.pose = "G"
+            return 8
+        raise MealyError("mix")
+
+    def hike(self):
+        if self.pose == "A":
+            self.pose = "C"
+            return 1
+        if self.pose == "E":
+            self.pose = "F"
             return 6
-        raise MealyError("order")
+        if self.pose == "F":
+            return 10
+        if self.pose == "G":
+            self.pose = "H"
+            return 11
+        raise MealyError("hike")
+
+    def rig(self):
+        if self.pose == "C":
+            self.pose = "D"
+            return 4
+        if self.pose == "D":
+            self.pose = "E"
+            return 5
+        if self.pose == "F":
+            self.pose = "H"
+            return 9
+        if self.pose == "B":
+            self.pose = "H"
+            return 3
+        raise MealyError("rig")
 
 
 def main():
-    return StateMachine
+    return StateMachine()
 
 
 def raises(func, error):
@@ -61,8 +66,40 @@ def raises(func, error):
 
 def test():
     o1 = main()
-    raises(lambda: o1.stay(), MealyError)
-    raises(lambda:  o1.order(), MealyError)
-    assert
+    raises(lambda: o1.rig(), MealyError)
+    assert o1.mix() == 0 and o1.pose == "B"
+    raises(lambda: o1.hike(), MealyError)
+    assert o1.mix() == 2 and o1.pose == "C"
+    raises(lambda: o1.mix(), MealyError)
+    raises(lambda: o1.hike(), MealyError)
+    assert o1.rig() == 4 and o1.pose == "D"
+    raises(lambda: o1.mix(), MealyError)
+    raises(lambda: o1.hike(), MealyError)
+    assert o1.rig() == 5 and o1.pose == "E"
+    raises(lambda: o1.rig(), MealyError)
+    assert o1.hike() == 6 and o1.pose == "F"
+    assert o1.hike() == 10 and o1.pose == "F"
+    assert o1.mix() == 8 and o1.pose == "G"
+    raises(lambda: o1.mix(), MealyError)
+    raises(lambda: o1.rig(), MealyError)
+    assert o1.hike() == 11 and o1.pose == "H"
+    raises(lambda: o1.mix(), MealyError)
+    raises(lambda: o1.rig(), MealyError)
+    raises(lambda: o1.hike(), MealyError)
+
+    o2 = main()
+    assert o2.hike() == 1 and o2.pose == "C"
+    o2.rig()
+    o2.rig()
+    assert o2.mix() == 7 and o2.pose == "C"
+    o2.rig()
+    o2.rig()
+    o2.hike()
+    assert o2.rig() == 9 and o2.pose == "H"
+
+    o3 = main()
+    o3.mix()
+    assert o3.rig() == 3 and o3.pose == "H"
 
 
+test()
